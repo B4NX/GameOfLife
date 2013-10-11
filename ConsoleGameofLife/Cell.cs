6879eebd2718cccf05cbc25c287f653a6f18b5f4
B4NX <nikko.rush@gmail.com>
2013-10-11 15:54:55 -0700

    Cell changes to create neighbor list

--- ConsoleGameofLife/Cell.cs
+++ ConsoleGameofLife/Cell.cs
@@ -2,18 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.IO;
+//using System.IO;
 
 namespace ConsoleGameofLife {
     public class Cell {
 
         private bool isAlive;
         private int row, col;
+        private List<Tuple<int, int>> neighbors;
 
         public Cell(bool isAlive, int r, int c) {
             this.isAlive = isAlive;
             this.row = r;
             this.col = c;
+            this.neighbors = getNeighbors(r,c);
+        }
+        private static List<Tuple<int,int>> getNeighbors(int r, int c){
+            List<Tuple<int, int>> temp = new List<Tuple<int, int>>();
+
+            for (int row = r-1; row <= r+1; row++)
+            {
+                for (int col = c-1; col <= c+1; col++)
+                {
+                    if (row == 0 && col == 0)
+                    {
+                        continue;
+                    }
+                    if (r < 0 || c < 0)
+                    {
+                        continue;
+                    }
+                    if (r > Grid.grid.GetLength(0) || c > Grid.grid.GetLength(1))
+                    {
+                        continue;
+                    }
+
+                    temp.Add(new Tuple<int, int>(row, col));
+                }
+            }
+            return temp;
         }
 
         public void Update() {
@@ -33,17 +60,9 @@ namespace ConsoleGameofLife {
 
             for (int i = row - 1; i <= row + 1; i++) {
                 for (int j = col - 1; j <= col + 1; j++) {
-                    try
+                    if (Grid.grid[i, j] != this && Grid.grid[i, j].IsAlive)
                     {
-                        if (Grid.grid[i, j] != this && Grid.grid[i, j].IsAlive)
-                        {
-                            alive++;
-                        }
-                    }
-                    catch (IndexOutOfRangeException e)
-                    {
-                        System.Diagnostics.Debug.WriteLine(e);
-                        continue;
+                        alive--;
                     }
                 }
             }
